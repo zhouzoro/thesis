5f918e2598d24fa582ea691b8d9f86405203d502
Jason Zhou <zyol@outlook.com>
2016-05-06 04:49:20 +0800

    0506

--- wwwroot/javascripts/core.js
+++ wwwroot/javascripts/core.js
@@ -1,7 +1,7 @@
 require([
     "esri/Map",
     "esri/views/MapView",
-    "esri/layers/ArcGISDynamicLayer",
+    "esri/layers/MapImageLayer",
     "esri/layers/FeatureLayer",
     "esri/PopupTemplate",
     "esri/widgets/Legend",
@@ -9,8 +9,13 @@ require([
     "esri/widgets/Compass/CompassViewModel",
     "esri/widgets/Home",
     "esri/widgets/Home/HomeViewModel",
+    "esri/widgets/BasemapToggle",
+    "esri/widgets/Popup",
+    "esri/tasks/QueryTask",
+    "esri/tasks/support/Query",
     "dojo/domReady!"
-], function(Map, MapView, ArcGISDynamicLayer, FeatureLayer, PopupTemplate, Legend, Compass, CompassVM, Home, HomeVM) {
+], function(Map, MapView, MapImageLayer, FeatureLayer, PopupTemplate, Legend, Compass, CompassVM, Home, HomeVM, BasemapToggle, Popup, QueryTask, Query) {
+
     var map = new Map({
         basemap: "oceans"
     });
@@ -19,169 +24,230 @@ require([
         container: "map-container",
         map: map,
         zoom: 9,
-        center: [121.5, 33]
+        center: [121, 33]
     });
+    var services = ['zoning', 'authorized', 'authorized_source', 'authorized_cata1', 'authorized_cata2', 'authorized_method'];
+    view.then(function() {
 
-    /*var lyr = new ArcGISDynamicLayer({
-        url: "http://localhost:6080/arcgis/rest/services/04202/MapServer"
-    });
-    map.add(lyr);*/
+        var content = "<b>{FID}</b> {OBJECTID}<br>" +
+            "功能区类型:{功能区类型}<br>" + "{SHAPE_LEN}" + "<img src='https://www.google.com/logos/doodles/2016/mohammed-ghani-hikmats-87th-birthday-5708620060688384-hp2x.jpg' />";
+        var tableContent = '<table class="ui celled table"><tbody><tr><td>' + 'FID' + '</td><td>' + '{FID}' + '</td></tr><tr><td>' + '配号来源' + '</td><td>' + '{配号来源}' + '</td></tr><tr><td>' + '用海一级类' + '</td><td>' + '{用海一级类}' + '</td></tr><tr><td>' + '用海二级类' + '</td><td>' + '{用海二级类}' + '</td></tr><tr><td>' + '用海方式' + '</td><td>' + '{用海方式}' + '</td></tr></tbody></table>'
 
-    var content = "<b>{FID}</b> {OBJECTID}<br>" +
-        "功能区类型:{功能区类型}<br>" + "{SHAPE_LEN}" + "<img src='https://www.google.com/logos/doodles/2016/mohammed-ghani-hikmats-87th-birthday-5708620060688384-hp2x.jpg' />";
-
-    var pt = new PopupTemplate({
-        title: '{FID}',
-        content: content
-    });
-    var legendJson = {};
-    $.get('http://localhost:6080/arcgis/rest/services/04202/MapServer/legend?f=pjson', function(res) {
-        legendJson =    res;
-    })
-    var zoningLyr = new FeatureLayer({
-        id: '功能区划',
-        url: "http://localhost:6080/arcgis/rest/services/04202/MapServer/5",
-        //popupTemplate: pt
-    });
-    map.add(zoningLyr);
-    var tempLyr = new FeatureLayer({
-        url: "http://localhost:6080/arcgis/rest/services/04202/MapServer/2",
-        definitionExpression: "用海一级类 = '渔业用海'",
-            popupTemplate: new PopupTemplate({
-                title: 'title',
-                content: "layer.No." +  "<b>{FID}</b>"
+        //==widgets======================//
+        var compass = new Compass({
+            viewModel: new CompassVM({
+                view: view
             })
-    });
-    map.add(tempLyr);
-    /**/
-    var compass = new Compass({
-        viewModel: new CompassVM({
-            view: view
-        })
-    }, "compass");
-    compass.startup();
+        }, "compass");
+        compass.startup();
 
-    var homeBtn = new Home({
-        viewModel: new HomeVM({
-            view: view
-        })
-    }, "home");
-    homeBtn.startup();
-
-    var legend = new Legend({ view: view }, 'legend');
-    legend.startup();
-    var switchLyr = function() {
-        var lyrNames = ['确权数据', '确权数据--配号来源', '确权数据--用海一级类', '确权数据--用海二级类', '确权数据--用海方式', '功能区划'];
-        var authLyr = new FeatureLayer({
-            id: lyrNames[0],
-            url: "http://localhost:6080/arcgis/rest/services/04202/MapServer/0",
-            popupTemplate: new PopupTemplate({
-                title: 'title',
-                content: "layer.No." + i + "<b>{FID}</b>"
+        var homeBtn = new Home({
+            viewModel: new HomeVM({
+                view: view
             })
+        }, "home");
+        homeBtn.startup();
+
+        var basemapToggle = new BasemapToggle({
+            view: view,
+            nextBasemap: "dark-gray"
+        }, 'basemap');
+        basemapToggle.startup();
+
+        //var legend = new Legend({ view: view }, 'legend');
+
+        var legend = new Legend({
+            view: view
         });
-        map.add(authLyr);
+        legend.startup();
 
-        var lyrs = {};
-        lyrs[authLyr.id] = authLyr;
-        for (var i = 1; i < 5; i++) {
-            var newLyr = new FeatureLayer({
-                id: lyrNames[i],
-                url: "http://localhost:6080/arcgis/rest/services/04202/MapServer/" + i,
+        view.ui.add(legend, "bottom-right");
+        //==widgets======================//
+
+
+        var authLyrCtrl = function() {
+
+            var lyrIds = ['功能区划', '确权数据', '确权数据--配号来源', '确权数据--用海一级类', '确权数据--用海二级类', '确权数据--用海方式'];
+            var authLyrs = [];
+            authLyrs[1] = new FeatureLayer({
+                id: lyrIds[1],
+                url: "http://localhost:6080/arcgis/rest/services/allofit/MapServer/1",
                 popupTemplate: new PopupTemplate({
                     title: 'title',
-                    content: "layer.No." + i + "<br><b>{FID}</b>{用海一级类}"
+                    content: tableContent
                 })
             });
-            //add but do not show
-            newLyr.visible = false;
-            map.add(newLyr);
-            lyrs[newLyr.id] = newLyr;
-        }
-        return {
-            to: function to(val) {
-                for (var lyrid in lyrs) {
-                    lyrs[lyrid].visible = false;
-                    if (lyrid == lyrNames[val]) {
-                        lyrs[lyrid].visible = true;
-                    }
-                }
-                legend.refresh();
-                //layerControl.setCheckbox();
-            },
-            show: function hide(id) {
-                lyrs[id].visible = true;
-                legend.refresh();
-                layerControl.setCheckbox();
-            },
-            hide: function hide(id) {
-                lyrs[id].visible = false;
-                legend.refresh();
-                layerControl.setCheckbox();
+            map.add(authLyrs[1]);
+            var inviewIndex = 1;
+            for (var i = 2; i < 6; i++) {
+                var tempLyr = new FeatureLayer({
+                    id: lyrIds[i],
+                    url: "http://localhost:6080/arcgis/rest/services/allofit/MapServer/" + i,
+                    popupTemplate: new PopupTemplate({
+                        title: 'title',
+                        content: tableContent
+                    })
+                });
+                authLyrs.push(tempLyr);
             }
-        }
-    }();
 
-    $('#aythLyrCata').change(function() {
-        switchLyr.to($(this).val());
-    });
+            view.whenLayerView(authLyrs[1]).then(function(lyrView) {
+                lyrView.watch("updating", function(val) {
+                    if (!val) { // wait for the layer view to finish updating
 
+                        // query all the features available for drawing.
+                        lyrView.queryFeatures().then(function(results) {
+                            console.log(results[0]);
 
+                            /*graphics = results;
+                        var fragment = document.createDocumentFragment();
 
-    var layerControl = function() {
-        var specificLyrs = {};
-        var lyrNames = ['确权数据', '确权数据--配号来源', '确权数据--用海一级类', '确权数据--用海二级类', '确权数据--用海方式', '功能区划'];
+                        results.forEach(function(result, index) {
+                          var attributes = result.attributes;
+                          var name = attributes.ZIP + " (" +
+                            attributes.PO_NAME + ")"
 
-        function setCheckbox() {
-            $('.esri-legend-service').each(function() {
-                var legendSvcLyr = $(this);
-                var label = legendSvcLyr.find('.esri-legend-service-label').text();
-                var lyrName = label.substring(label.lastIndexOf(' ') + 1);
-                console.log(lyrName);
-                var layer = legendSvcLyr.find('.esri-legend-layer').first();
-                //loop through table rows
-                layer.find('tr').each(function() {
-                    var tr = $(this);
-                    var tds = tr.find('td');
-                    if (tds && tds.length && tds.length > 1) {
-                        var sym = $(this).find('td').first();
-                        //find the cell has text, which should be catagory info
-                        $(this).find('td').each(function() {
-                            var td = $(this);
-                            if (!(td.find('table')[0])) {
-                                var cata = $(this).text();
-                                if (cata && cata.length > 0) {
-                                    //add the layer
-                                    var lyrIndex = lyrNames.indexOf(lyrName);
-                                    var tempLyr = new FeatureLayer({
-                                        id: lyrName + '_' + cata,
-                                        url: "http://localhost:6080/arcgis/rest/services/04202/MapServer/" + lyrIndex,
-                                        definitionExpression: lyrName + " = " + "'" + cata + "'"
-                                    });
-                                    map.add(tempLyr);
-                                    specificLyrs[tempLyr.id] = tempLyr;
-                                    //add checkbox
-                                    var checkBox = $('<input>').attr({ 'type': 'checkbox', 'data-lyr': lyrName, 'data-cata': cata, 'data-lyrid': lyrName + '_' + cata }).prop('checked', true).text(cata).change(function() {
-                                        var lyrid = $(this).data('lyrid');
-                                        if ($(this).is(':checked')) {
-                                            specificLyrs[lyrid].visible = true;
-                                            return;
-                                        }
-                                        specificLyrs[lyrid].visible = false;
-                                    });
-                                    var newtd = $('<td>').append(checkBox);
-                                    //insert checkbox
-                                    newtd.insertBefore(sym);
-                                }
-                            }
+                          // Create a list zip codes in NY
+                          domConstruct.create("li", {
+                            className: "panel-result",
+                            tabIndex: 0,
+                            "data-result-id": index,
+                            textContent: name
+                          }, fragment);
+                        });
+                        domConstruct.place(fragment, listNode, "only");*/
                         });
                     }
                 });
-
             });
+            /*on(listNode, on.selector("li", a11yclick), function(evt) {
+                var target = evt.target;
+                var resultId = domAttr.get(target, "data-result-id");
+
+                // get the graphic corresponding to the clicked zip code
+                var result = resultId && graphics && graphics[parseInt(resultId,
+                    10)];
+
+                if (result) {
+                    // open the popup at the centroid of zip code polygon
+                    // and set the popup's features which will populate popup content and title.
+                    view.popup.open({
+                        features: [result],
+                        location: result.geometry.centroid
+                    });
+                }
+            });*/
+
+            return {
+                on: true,
+                switchTo: function to(val) {
+                    if (val !== inviewIndex) {
+                        map.remove(authLyrs[inviewIndex]);
+                        map.add(authLyrs[val]);
+                    }
+                },
+                show: function() {
+                    map.add(authLyrs[inviewIndex]);
+                    this.on = true;
+                },
+                hide: function() {
+                    map.remove(authLyrs[inviewIndex]);
+                    this.on = false;
+                }
+            }
+        }();
+
+
+        var zoningLyrCtrl = function() {
+
+            var zoningLyr = new FeatureLayer({
+                id: '功能区划',
+                url: "http://localhost:6080/arcgis/rest/services/zoning/MapServer/0",
+            });
+
+            return {
+                on: false,
+                toggle: function() {
+                    if (this.on) {
+                        map.remove(zoningLyr);
+                    } else {
+
+                        map.add(zoningLyr);
+                        if (authLyrCtrl.on) {
+                            authLyrCtrl.hide;
+                            authLyrCtrl.show;
+
+                        }
+                    };
+                    this.on = !this.on;
+                }
+            }
+        }();
+
+        $('input.toggle.toggle-zoning').change(function() {
+            zoningLyrCtrl.toggle();
+            console.log(zoningLyrCtrl.on)
+        })
+
+        $('input.toggle.toggle-zoning').change(function() {
+            if ($(this).val() == 'on') {
+                authLyrCtrl.show;
+            } else {
+                authLyrCtrl.hide;
+
+            }
+        })
+        $('select[name=authLyr]').change(function() {
+            authLyrCtrl.switchTo($(this).val());
+        });
+
+        function dataSrc() {
+            this.chart = {
+                "caption": "确权数据分类",
+                "subCaption": "用海一级类",
+                "xAxisName": "用海一级类",
+                "yAxisName": "数量",
+                "theme": "fint"
+            };
+            this.data = [];
         }
-        return {
-            setCheckbox: setCheckbox
-        }
-    }();
+        var col3dShow = [];
+
+        /*$.get('http://localhost:6080/arcgis/rest/services/allofit/MapServer/legend?f=pjson', function(res) {
+            legendJson = JSON.parse(res);
+            for (var i = 1; i < 6; i++) {
+                var layerTemp = legendJson.layers[i];
+                var layerLegend = layerTemp.legend;
+                var tempSrc = new dataSrc();
+                tempSrc.chart.subCaption = layerTemp.layerName;
+                tempSrc.chart.xAxisName = layerTemp.layerName;
+                var queryStatesTask = new QueryTask({
+                    url: "http://localhost:6080/arcgis/rest/services/allofit/MapServer/" + i
+                });
+                var promiseResults = [];
+                for (var j = layerLegend.length - 1; i >= 0; i--) {
+                    var legd = layerLegend[i];
+                    promiseResults.push(new Promise((resolve, reject) => {
+                        var query = new Query();
+                        query.where = layerTemp.layerName + " = " + "'" + legd.label + "'";
+                        queryStatesTask.execute(query).then(function(result) {
+                            console.log(result.features.length);
+                            var tempData = {
+                                "label": legd.label,
+                                "value": result.features.length
+                            };
+                            resolve(tempData);
+                        });
+                    }))
+                }
+                Promise.all(promiseResults).then(function(dataArr) {
+                    console.log(dataArr);
+                    tempSrc.data = dataArr;
+                    renderCol3d(tempSrc);
+
+                })
+            }
+        })*/
+    });
+
 });
